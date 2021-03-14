5e041ef79529c92f7696b22989454512da30dfbe
arianecrestani <ariane.mrv@gmail.com>
2021-03-14 16:29:01 +0100

    clean the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'production',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
