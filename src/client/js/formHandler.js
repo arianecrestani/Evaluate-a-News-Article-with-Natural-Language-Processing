e58fed1aafe342e8e174c1b13b9f1ffa40923126
arianecrestani <ariane.mrv@gmail.com>
2021-05-28 19:00:13 +0200

    enter press and cleaning input

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,13 +2,12 @@ import { wordsCount } from "./wordsCount";
 
 function handleSubmit(event) {
   event.preventDefault();
-
+  
   // check what text was put into the form field
-  let textField = document.getElementById("name").value ;
-
+  let textField = document.getElementById("name").value;
 
   if (Client.wordsCount(textField) < 3) {
-    alert("need to be more than 3 words")
+    alert("need to be more than 3 words");
     return;
   }
 
@@ -21,33 +20,38 @@ function handleSubmit(event) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ textField }), // body data type must match "Content-Type" header
-  }
-  
+  };
+
   fetch("http://localhost:8081/submit", request)
     .then((result) => result.json())
     .then((json) => {
       updateUI(formatedData(json));
       console.log(json);
     });
-
+  
+  // cleaning input
+  document.getElementById("name").value = "";
+  //enter press event
+  const enterPress = (e) => {
+    if (e.key === "Enter") {
+  
+      textField.addEventListener("keypress", handleSubmit);
+    }
+    enterPress()
+  };
 
 }
 
 function updateUI(res) {
-  console.log(res);
-  const irony = document.getElementById("irony");
   const score = document.getElementById("score");
   const confidence = document.getElementById("confidence");
 
-  irony.innerHTML = res.irony ? res.irony : "";
   score.innerHTML = `Polarity score: ${res.score}`;
-  confidence.innerHTML = `Confidence: ${res.confidence}`+ '%';
-
+  confidence.innerHTML = `Confidence: ${res.confidence}` + "%";
 
 }
 
 const formatedData = (data) => {
-  
   let score_text = data.score_tag;
 
   if (data.score_tag === "P+") {
@@ -62,20 +66,14 @@ const formatedData = (data) => {
     score_text = " strong negative";
   } else if (data.score_tag === "NONE") {
     score_text = "without sentiment";
-  }   
+  }
 
   const result = {
-    "irony": data.irony,  
-    "score":  score_text,
-    "confidence": data.confidence
+    score: score_text,
+    confidence: data.confidence,
+  };
 
-  
-  }
-  
   return result;
-
-
 };
 
 export { handleSubmit };
-
--- src/client/js/wordsCount.js
+++ src/client/js/wordsCount.js
@@ -1,6 +1,6 @@
 const wordsCount = (inputText) => {
   let wordCount = 0;
-  
+
   inputText.split(" ").forEach((element) => {
     if (element !== "") {
       wordCount++;
